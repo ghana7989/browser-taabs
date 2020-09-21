115946839e8e50487672a0e2387f70a08c048ae9
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-21 10:38:17 +0530

    Updates

---
+++ precache-manifest.f24d17c5fcedb5552cd193f71408991b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "645a606a9e56c7b001d79a9b5f8407e4",
+    "url": "/browser-tabs/index.html"
+  },
+  {
+    "revision": "b95838df536416228d5a",
+    "url": "/browser-tabs/static/css/main.ddc2df62.chunk.css"
+  },
+  {
+    "revision": "d8a77d2369bdb3db3384",
+    "url": "/browser-tabs/static/js/2.d96b0829.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/browser-tabs/static/js/2.d96b0829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95838df536416228d5a",
+    "url": "/browser-tabs/static/js/main.af175550.chunk.js"
+  },
+  {
+    "revision": "b96be20b50ca00c9c969",
+    "url": "/browser-tabs/static/js/runtime-main.f2b303d1.js"
+  }
+]);
